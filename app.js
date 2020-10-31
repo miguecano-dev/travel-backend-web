9413ad94101e5c08669865931687cabfcaad571f
Fabian Pena <camilo.fabis@gmail.com>
2020-10-31 14:40:03 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const bodyParser = require('body-parser');
+const createError = require('http-errors');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/travel'
+console.log('Connecting DB to ', MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error('Error connecting to mongo', err));
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/City.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+console.log('hmmm')
+const citySchema = new Schema(
+  {
+    city: {
+      type: String
+    },
+    city_ascii: {
+      type: String
+    },
+    country: {
+      type: String
+    },
+    img: {
+      type: String,
+      default: "https://busites-www.s3.amazonaws.com/blog-margaritaville/2018/03/travel-generic-shot.jpg"
+    },
+    iso2: {
+      type: String
+    },
+    admin_name: {
+      type: String
+    },
+    capital: {
+      type: String
+    },
+    population: {
+      type: Number
+    },
+    id: {
+      type: String
+    },
+    loc: {
+      type: { type: String }
+      , coordinates: []
+    },
+    posts: { 
+      type: [{ type: Schema.Types.ObjectId, ref: "Post" }] 
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+const City = model('City', citySchema);
+module.exports = City;
